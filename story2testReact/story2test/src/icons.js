78f9e1f55b9e8b28716ce6bdc4aa37cc0d5081af
Simoes <pedrosimoes.ps@hotmail.com>
2024-11-13 13:03:19 +0000

    Initial react project

---
+++ story2testReact/story2test/src/icons.js
@@ -0,0 +1,21 @@
+import convert_to_form from './assets/convert-to-form.svg';
+import attach_file from './assets/attach-file.svg';
+import send_circle from './assets/send-circle.svg';
+import right_arrow from './assets/right-arrow.svg';
+import left_arrow from './assets/left-arrow.svg';
+import add_circle from './assets/add-circle.svg';
+import new_chat from './assets/new-chat.svg';
+import help_circle from './assets/help-circle.svg';
+import settings from './assets/settings.svg';
+
+export {
+  convert_to_form,
+  attach_file,
+  send_circle,
+  right_arrow,
+  left_arrow,
+  add_circle,
+  new_chat,
+  help_circle,
+  settings,
+};
